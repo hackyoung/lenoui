6b3b09f19542808993e4415ab2b9b3e515990c10
hackyoung <hackyoung@163.com>
2016-07-25 18:47:29 +0800

    add lenojs

---
+++ js/lenoui.js
@@ -0,0 +1,70 @@
+(function($) {
+    function openModal($modal) {
+        var on = $modal.data('onOpen');
+        if (typeof on === 'function') {
+            on($modal);
+        }
+        $modal.addClass('show');
+    }
+    function closeModal($modal) {
+        var on = $modal.data('onClose');
+        if (typeof on === 'function') {
+            on($modal);
+        }
+        $modal.removeClass('show');
+    }
+    function registerOnClose($modal, onclose) {
+        if (typeof onclose !== 'function') {
+            throw 'onclose not a function';
+        }
+        $modal.data('onClose', onclose);
+    }
+    function registerOnOpen($modal, onopen) {
+        if (typeof onopen !== 'function') {
+            throw 'onclose not a function';
+        }
+        $modal.data('onOpen', onopen);
+    }
+    $.fn.luiModal = function(opt, callback) {
+        switch(opt) {
+            case 'modal.open':
+                return openModal($(this));
+            case 'modal.close':
+                return closeModal($(this));
+            case 'modal.on.close':
+                return registerOnClose($(this), callback);
+            case 'modal.on.open':
+                return registerOnOpen($(this), callback);
+        }
+    }
+})(jQuery);
+
+$(function() {
+    /**
+     * 初始化modal-toggle
+     */
+    $('[data-toggle=modal]').click(function() {
+        var selector = $(this).attr('for');
+        $(selector).luiModal('modal.open');
+    });
+    /**
+     * 初始化modal
+     */
+    $('.modal').find('.close').click(function() {
+        var $p = $(this).parent();
+        while(!$p.hasClass('modal')) {
+            $p = $p.parent();
+        }
+        $p.luiModal('modal.close');
+    });
+
+    /**
+     * 初始化tab
+     */
+    $('[data-toggle=tab]').click(function() {
+        var $self = $(this);
+        var selector = $self.attr('for');
+        $self.addClass('active').siblings().removeClass('active');
+        $('#'+selector).addClass('active').siblings().removeClass('active');
+    });
+});
